75e3c8c29d63b49abe225939b238ae4bdacab467
Waidhoferj <jwaidhof@calpoly.edu>
2018-11-10 22:36:05 -0800

    got search module working

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 const {getData} = require('./data.js')
 const {Product} = require('./model.js')
-getData()
+const algoliasearch = require('algoliasearch')
+const client = algoliasearch('T3URAZ0NDB', '426c213d71fafe032db3ed25bf49fd3b');
+const index = client.initIndex('products');
+
+
 
